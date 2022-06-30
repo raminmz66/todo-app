8c4c3ab35d4c24d9ddccc800dc9dce556d5e9e04
Ramin <ramin.mz66@gmail.com>
2022-06-30 18:32:02 +0430

    feat: added deployment to github pages

---
+++ scripts/github-pages-deploy.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+const execa = require("execa");
+const fs = require("fs");
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "github-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "github-pages"]);
+    console.log("Pushing to github-pages...");
+    await execa("git", ["push", "origin", "HEAD:github-pages", "--force"]);
+    await execa("rm", ["-r", folderName]);
+    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["branch", "-D", "github-pages"]);
+    console.log("Successfully deployed, check your settings");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/todo-app' : '',
+  base: process.env.NODE_ENV === 'production' ? '/todo-app/' : '',
 })
