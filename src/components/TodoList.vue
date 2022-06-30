432b8f0925d270af6ebcd9857eda6d2b8a3c4592
Ramin <ramin.mz66@gmail.com>
2022-06-30 21:53:15 +0430

    feat: added simple todo

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            {{ todo.text }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'TodoList',
+    props: {
+        todos: Array
+    }
+}
+</script>
+
+<style>
+    
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,27 @@
 <script>
+import AddTodo from '../components/AddTodo.vue';
+import TodoList from '../components/TodoList.vue';
 export default {
-    name: 'HomeView'
+    name: "HomeView",
+    components: { AddTodo, TodoList },
+    data() {
+        return {
+            items: []
+        }
+    },
+    methods: {
+        addNote(data) {
+            this.items = [{ text: data.text }, ...this.items];
+        }
+    }
 }
 </script>
 
 <template>
     <div class="container">
         <h1>Let's start buidling a great app! :D</h1>
+        <AddTodo @add="addNote"></AddTodo>
+        <TodoList :todos="items"></TodoList>
     </div>
 </template>
 
