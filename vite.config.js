2a8b5c0e8cd9bb515ce1a1a5ea592a5d1dc7bd2d
Ramin <ramin.mz66@gmail.com>
2022-06-30 18:23:49 +0430

    feat: added new url for deployment to github pages

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/todo-app' : '',
 })
