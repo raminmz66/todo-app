4237905680e628fe8e0dfbd9f13dc7c31a46ef3e
Ramin <ramin.mz66@gmail.com>
2022-07-24 19:40:00 +0430

    feat: added left panel basic structure

--- src/components/TheLeftPanel.vue
+++ src/components/TheLeftPanel.vue
@@ -1,8 +1,27 @@
 <template>
-    <f7-panel left push>
+    <f7-panel left push backdrop>
       <f7-view>
-        <f7-page>
-          <f7-block>Left panel content</f7-block>
+        <f7-page bgColor="white">
+          <f7-list menu-list>
+            <f7-list-item
+              link
+              title="Home"
+              :selected="true"
+              @click="() => (selected = 'home')"
+            >
+              <template #media>
+                <f7-icon md="material:home" aurora="f7:house_fill" ios="f7:house_fill" />
+              </template>
+            </f7-list-item>
+            <f7-list-item
+              link
+              title="About"
+            >
+              <template #media>
+                <f7-icon md="material:person" aurora="f7:person_fill" ios="f7:person_fill" />
+              </template>
+            </f7-list-item>
+          </f7-list>
         </f7-page>
       </f7-view>
     </f7-panel>
@@ -15,5 +34,5 @@ export default {
 </script>
 
 <style>
-    
+
 </style>
