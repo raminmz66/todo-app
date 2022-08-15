bb13702b6c66981bd1e1742bb3ce2a911232a912
Ramin <ramin.mz66@gmail.com>
2022-08-15 17:49:46 +0430

    fix: fixed manifest

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,33 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        "name": "Todo app",
+        "short_name": "Todo app",
+        "description": "A simple todo app, just for you!",
+        "icons": [
+            {
+                "src": "/android-chrome-192x192.png",
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": "/android-chrome-512x512.png",
+                "sizes": "512x512",
+                "type": "image/png"
+            }
+        ],
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "display": "standalone"
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
