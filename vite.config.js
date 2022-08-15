a875ba909311a9402f7c4503c7d8ae0a2a099aa5
Ramin <ramin.mz66@gmail.com>
2022-08-15 17:59:49 +0430

    fix: fixed url

--- vite.config.js
+++ vite.config.js
@@ -39,5 +39,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production_' ? '/todo-app/' : '',
+  base: process.env.NODE_ENV === 'production' ? '/todo-app/' : '',
 })
