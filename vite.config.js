bc9ae3e2b02125b9df6af55a80bdcdd88e838f7f
Ramin <ramin.mz66@gmail.com>
2022-08-15 18:55:24 +0430

    fix: fixed icons problem

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         "description": "A simple todo app, just for you!",
         "icons": [
             {
-                "src": "/android-chrome-192x192.png",
+                "src": "android-chrome-192x192.png",
                 "sizes": "192x192",
                 "type": "image/png"
             },
             {
-                "src": "/android-chrome-512x512.png",
+                "src": "android-chrome-512x512.png",
                 "sizes": "512x512",
                 "type": "image/png"
             }
