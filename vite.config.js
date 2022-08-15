25c362b517b45ac0757516128e420ae1a592e485
Ramin <ramin.mz66@gmail.com>
2022-08-15 15:30:30 +0430

    feat: added manifest and asset files

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/todo-app/' : '',
+  base: process.env.NODE_ENV === 'production_' ? '/todo-app/' : '',
 })
